4b98072a88d180f828fe65357b520ec3d904f784
Katerina <ekaterina.tereshko.me@gmail.com>
2023-11-09 19:29:40 +0300

    new HTML structure and designs added

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ particlesJS("particles-js",
             "value": 1000,
             "density": {
                 "enable": true,
-                "value_area":800
+                "value_area":1800
             }
         },
         "color": {
